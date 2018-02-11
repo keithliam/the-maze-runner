79ceaffc82dc956f143c958607b56df04e9048a0
Keith Liam Manaoto <keithliamm@gmail.com>
2018-02-11 14:44:39 +0800

    Update GUI

--- Game.java
+++ Game.java
@@ -1,15 +1,20 @@
 package themazerunner;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
 import java.awt.EventQueue;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Image;
-import javax.imageio.ImageIO;
 
 import java.util.Random;
 
@@ -87,6 +92,75 @@ public class Game extends JPanel {
 			public void keyTyped(KeyEvent e){}
 			public void keyReleased(KeyEvent e){}
 		});
+
+		ImageIcon icon1 = new ImageIcon("img/tiles.png");
+		JLabel bottomPanel1 = new JLabel();
+		JLabel bottomPanel2 = new JLabel();
+		JLabel bottomPanel3 = new JLabel();
+		JLabel bottomPanel4 = new JLabel();
+		JLabel bottomPanel5 = new JLabel();
+		JLabel bottomPanel6 = new JLabel();
+		JLabel bottomPanel7 = new JLabel();
+		JLabel bottomPanel8 = new JLabel();
+		JLabel bottomPanel9 = new JLabel();
+		JLabel bottomPanel10 = new JLabel();
+		bottomPanel1.setBounds(0, 600, 60, 60);
+		bottomPanel2.setBounds(60, 600, 60, 60);
+		bottomPanel3.setBounds(120, 600, 60, 60);
+		bottomPanel4.setBounds(180, 600, 60, 60);
+		bottomPanel5.setBounds(240, 600, 60, 60);
+		bottomPanel6.setBounds(300, 600, 60, 60);
+		bottomPanel7.setBounds(360, 600, 60, 60);
+		bottomPanel8.setBounds(420, 600, 60, 60);
+		bottomPanel9.setBounds(480, 600, 60, 60);
+		bottomPanel10.setBounds(540, 600, 60, 60);
+		bottomPanel1.setIcon(icon1);
+		bottomPanel2.setIcon(icon1);
+		bottomPanel3.setIcon(icon1);
+		bottomPanel4.setIcon(icon1);
+		bottomPanel5.setIcon(icon1);
+		bottomPanel6.setIcon(icon1);
+		bottomPanel7.setIcon(icon1);
+		bottomPanel8.setIcon(icon1);
+		bottomPanel9.setIcon(icon1);
+		bottomPanel10.setIcon(icon1);
+
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		ImageIcon left = new ImageIcon("img/left.png");
+		ImageIcon right = new ImageIcon("img/right.png");
+
+		button1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){}
+		});
+
+		button2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){}
+		});
+
+		button1.setBounds(250, 610, 40, 40);
+		button2.setBounds(310, 610, 40, 40);
+		button1.setIcon(left);
+		button2.setIcon(right);
+		button1.setContentAreaFilled(false);
+		button1.setFocusPainted(false);
+		button1.setBorderPainted(false);
+		button2.setContentAreaFilled(false);
+		button2.setFocusPainted(false);
+		button2.setBorderPainted(false);
+		this.add(button1);
+		this.add(button2);
+
+		this.add(bottomPanel1);
+		this.add(bottomPanel2);
+		this.add(bottomPanel3);
+		this.add(bottomPanel4);
+		this.add(bottomPanel5);
+		this.add(bottomPanel6);
+		this.add(bottomPanel7);
+		this.add(bottomPanel8);
+		this.add(bottomPanel9);
+		this.add(bottomPanel10);
 	}
 
 	public void move(int direction){
